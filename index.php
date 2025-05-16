81e5a8b8c8436dd7f6a4e0a3c66cc886ebae0237
EvilCPeach <glisssstt@gmail.com>
2025-05-16 14:55:03 +0200

    Сделать регистрацию и вход

--- index.php
+++ index.php
@@ -26,27 +26,73 @@
             <a href="#">Главная</a>
             <a href="#aboutUs">О нас</a>
             <a href="#portfolio">Портфолио</a>
-            <article class="dropdown-links">
-                <a href="#">Главная</a>
-                <a href="#aboutUs">О нас</a>
-                <a href="#portfolio">Портфолио</a>
-            </article>
+        </section>
+        <button class="buttonHamburger" id="buttonHamburger">
+            <img src="./images/free-icon-arrow-right-1549454.png" alt="">
+        </button>
+        <section class="dropdown-links" id="dropdown-links">
+            <a href="#">Главная</a>
+            <a href="#aboutUs">О нас</a>
+            <a href="#portfolio">Портфолио</a>
         </section>
         <section class="entry">
             <button class="profile" id="profile">
                 <img src="./images/profile_icon.png" alt="">
             </button>
             <article class="dropdown-profile" id="dropdown-profile">
-                <a href="#">Вход</a>
-                <a href="#">Регистрация</a>
+                <a href="#" id="profileEntry">Вход</a>
+                <a href="#" id="profileRegistration">Регистрация</a>
             </article>
         </section>
     </header>
-    <main class="ss">
-        
+    <main class="bgModal hidden" id="bgModal"></main>
+    <main class="parentModal hidden" id="parentModal">
+        <section class="modal" id="modal">
+            <article class="modal-top">
+                <div class="modalEntry">
+                    <p>Вход</p>
+                </div>
+                <div class="registration">
+                    <p>Регистрация</p>
+                </div>
+                <button class="closeModal" id="closeModal">
+                    <img src="./images/close-icon.png" alt="Закрыть">
+                </button>
+            </article>
+            <form action="" method="POST" class="entryForm">
+                <input type="email" placeholder="Введите почту" class="inputPadding" required>
+                <input type="password" placeholder="Введтие пароль" class="inputPadding" required>
+                <input type="submit" value="Войти">
+            </form>
+        </section>
+    </main>
+    <main class="slider">
+        <section class="images" id="images">
+            <article class="image">
+                
+            </article>
+            <article class="image">
+                
+            </article>
+            <article class="image">
+                
+            </article>
+        </section>
+        <button class="previous" id="previous">
+            <img src="./images/free-icon-arrow-right-1549454.png" alt="">
+        </button>
+        <button class="next" id="next">
+            <img src="./images/free-icon-arrow-right-1549454.png" alt="">
+        </button>
     </main>
     <footer>
-
+        <section class="footer-links">
+            <a href="#">Главная</a>
+            <a href="#aboutUs">О нас</a>
+            <a href="#portfolio">Портфолио</a>
+            <a href="#">Вверх</a>
+        </section>
+        <p>©Дронов Д.С. 2025 специально для ГАУ КО "Колледж предпринимательства"</p>
     </footer>
     <script src="./scriptes/script.js"></script>
 </body>
--- scriptes/script.js
+++ scriptes/script.js
@@ -1,15 +1,38 @@
 const header = document.getElementById('header');
+const dropdownLinks = document.getElementById('dropdown-links');
+const dropdownProfile = document.getElementById('dropdown-profile');
+let profileClicks = 0;
+const profile = document.getElementById('profile').addEventListener('click', function(event){
+    event.preventDefault();
+    dropdownLinks.style.opacity = '0';
+    profileClicks++;
+    dropdownProfile.style.opacity = '100';
+    if(profileClicks % 2 == 0){
+        dropdownProfile.style.opacity = '0';
+    }
+});
+let linksClicks = 0;
+const buttonHamburger = document.getElementById('buttonHamburger').addEventListener('click', function(){
+    linksClicks++;
+    dropdownProfile.style.opacity = '0';
+    dropdownLinks.style.opacity = '100';
+    if(linksClicks % 2 == 0){
+        dropdownLinks.style.opacity = '0';
+    }
+});
 document.addEventListener('scroll', function(){
+    dropdownProfile.style.opacity = '0';
+    dropdownLinks.style.opacity = '0';
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         header.style.backgroundColor = "#59a0e700";
     } else {
         header.style.backgroundColor = "#59a0e7";
     }
 });
+const preloader = document.getElementById('preloader');
+const spinner = document.getElementById('spinner');
+const body = document.querySelector('body');
 window.addEventListener('load', function() {
-    const preloader = document.getElementById('preloader');
-    const spinner = document.getElementById('spinner');
-    const body = document.querySelector('body');
     let interval = setInterval(() => {
         preloader.classList.add('hidden');
         spinner.classList.add('hidden');
@@ -19,7 +42,87 @@ window.addEventListener('load', function() {
         }
     }, 500);
 });
-const profile = document.getElementById('profile').addEventListener('click', function(event){
+const parentModal = document.getElementById('parentModal');
+const modal = document.getElementById('modal');
+const bgModal = document.getElementById('bgModal');
+const closeModal = document.getElementById('closeModal').addEventListener('click', function(){
+    modal.style.opacity = '0';
+    bgModal.classList.add('hidden');
+    body.classList.remove('notScroll');
+    parentModal.classList.remove('showModal');
+    parentModal.classList.add('hidden');
+});
+const profileEntry = document.getElementById('profileEntry').addEventListener('click', function(event){
     event.preventDefault();
-    document.getElementById('dropdown-profile').style.opacity = '100';
-})
+    let modalInterval = setInterval(() => {
+        modal.style.opacity = '100';
+        if(modal.style.opacity == '100'){
+            clearInterval(modalInterval);
+        }
+    }, 100)
+    bgModal.classList.remove('hidden');
+    body.classList.add('notScroll');
+    parentModal.classList.remove('hidden');
+    parentModal.classList.add('showModal');
+});
+let currentImage = 0;
+let startX = 0;
+let endX = 0;
+const buttonPrev = document.getElementById('previous');
+const buttonNext = document.getElementById('next');
+let images = document.getElementById('images');
+buttonPrev.addEventListener('click', () => {
+    imagesInterval = clearInterval(imagesInterval);
+    imagesInterval = setInterval(()=>{
+        changeImage(1);
+    }, 7000);
+    changeImage(-1);
+});
+buttonNext.addEventListener('click', () => {
+    imagesInterval = clearInterval(imagesInterval);
+    imagesInterval = setInterval(()=>{
+        changeImage(1);
+    }, 7000);
+    changeImage(1);
+});
+images.addEventListener('touchstart', (event) => {
+    startX = event.touches[0].clientX;
+    imagesInterval = clearInterval(imagesInterval);
+});
+images.addEventListener('touchmove', (event) => {
+    endX = event.touches[0].clientX;
+});
+images.addEventListener('touchend', () => {
+    swipe();
+    imagesInterval = setInterval(()=>{
+        changeImage(1);
+    }, 7000)
+});
+function swipe() {
+    const threshold = 50;
+    const distanceX = endX - startX;
+    if (distanceX > threshold) {
+        changeImage(-1);
+    } else if (distanceX < -threshold) {
+        changeImage(1);
+    }
+}
+function showImage(index) {
+    const slides = document.querySelectorAll('.image');
+    if (index >= slides.length) {
+        currentImage = 0;
+    } else if (index < 0) {
+        currentImage = slides.length - 1;
+    } else {
+        currentImage = index;
+    }
+    let offset = -currentImage * 33.4;
+    images.style.transform = `translateX(${offset}%)`;
+}
+
+function changeImage(direction) {
+    showImage(currentImage + direction);
+}
+let imagesInterval = setInterval(()=>{
+    changeImage(1);
+}, 7000);
